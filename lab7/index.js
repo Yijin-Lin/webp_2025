a766cbfee5462b6825c99f7f6e38d8fb95d716ba
Yijin-Lin <0515claricelin@gmail.com>
2025-05-07 01:02:32 +0800

    Add files via upload

---
+++ lab7/index.js
@@ -0,0 +1,15 @@
+var countainer=document.getElementById("Container");
+window.addEventListener("keyup",function(e){
+    console.log(e.key);
+    if(e.key){
+    //檢查第一個字元是否為e.key
+        first=countainer.innerHTML.charAt(0);
+        if 
+    //一樣的話把第一個字元消除
+    }
+    add_new_chars();
+});
+
+function add_new_chars(){
+    //container亂數增加一或三個字元
+}
